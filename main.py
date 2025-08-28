920b99fe56261b907b924117808b25f1dbf068cf
AumOzaa <programminglaptop1@gmail.com>
2025-08-28 16:16:05 +0530

    Completed main.py

--- main.py
+++ main.py
@@ -1,10 +1,53 @@
 from dotenv import load_dotenv
 from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_core.output_parsers import PydanticOutputParser
+from dotenv import load_dotenv
+from pydantic import BaseModel
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import PydanticOutputParser
+from langchain.agents import create_tool_calling_agent, AgentExecutor
 import os
 
+load_dotenv()   
 
-load_dotenv()
+class ResearchResponse(BaseModel):
+    topic: str
+    summary: str
+    sources: list[str]
+    tools_used: list[str]
+
 
 llm  = ChatGoogleGenerativeAI(model="gemini-1.5-flash",google_api_key=os.getenv("GEMINI_API_KEY"))
-response = llm.invoke("What is the meaning of coding all the day?!")
-print(response.content)
+parser = PydanticOutputParser(pydantic_object=ResearchResponse)
+
+prompt = ChatPromptTemplate.from_messages(
+    [
+        (
+            "system",
+            """
+            You are a research assistant that will help generate a research paper.
+            Answer the user query and use neccessary tools. 
+            Wrap the output in this format and provide no other text\n{format_instructions}
+            """,
+        ),
+        ("placeholder", "{chat_history}"),
+        ("human", "{query}"),
+        ("placeholder", "{agent_scratchpad}"),
+    ]
+).partial(format_instructions=parser.get_format_instructions())
+
+agent = create_tool_calling_agent(
+    llm=llm,
+    prompt=prompt,
+    tools=[]
+)
+
+agent_executor = AgentExecutor(agent=agent,tools=[],verbose=True) # Mark the verbose False if you don't want to see the CoT.
+raw_response = agent_executor.invoke({"query":"What is France known for?"})
+print(raw_response)
+
+try:
+    structured_response = parser.parse(raw_response.get("output")[0]["text"])
+    print(structured_response)
+except Exception as e:
+    print("Error parsing response", e, "Raw Response - ", raw_response)
---
+++ tools.py
@@ -0,0 +1,5 @@
+from langchain_community.tools import WikipediaQueryRun, DuckDuckGoSearchResults
+from langchain_community.utilities import WikipediaAPIWrapper
+from langchain.tools import Tool
+from datetime import datetime
+
